9ae7dd753b8d3e6064fd0fb8b3e6bd8fbc1023b2
Klap.ir <mohammadrezaasiyaban2286@gmail.com>
2023-01-25 19:34:08 +0330

    mamad

--- app/src/main/java/com/example/bmitest/MainActivity.java
+++ app/src/main/java/com/example/bmitest/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.bmitest;
 
+import static com.example.bmitest.R.color.yeloo;
+
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,40 +21,10 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-        binding.kaghaz.setOnClickListener(view -> {
-
-            String rockname = "rock";
-            String papername = "paper";
-            String scissorsname = "scissors";
-            String[] game = {rockname, papername, scissorsname};
-            String complay;
-            String yourplay=("paper");
-            int index;
-            binding.texsresr.setText(yourplay);
-            Random random = new Random();
-            index=random.nextInt(3);
-            complay=game[index];
-            binding.texsresr.setText("me     " + yourplay + "     bot      "    + complay);
-            if (yourplay.equals(complay)) {
-                binding.natije.setText("tin");
-            } else if (yourplay.equals(rockname) && complay.equals(papername)) {
-                binding.natije.setText("win bot");
-            } else if (yourplay.equals(rockname) && complay.equals(scissorsname)) {
-                binding.natije.setText("win");
-            } else if (yourplay.equals(papername) && complay.equals(rockname)) {
-                binding.natije.setText("win");
-            } else if (yourplay.equals(papername) && complay.equals(scissorsname)) {
-                binding.natije.setText("win bot");
-            } else if (yourplay.equals(scissorsname) && complay.equals(rockname)) {
-                binding.natije.setText("win bot");
-            } else if (yourplay.equals(scissorsname) && complay.equals(papername)) {
-                binding.natije.setText("win");
-            }
-
-        });
-
         binding.ghechi.setOnClickListener(view -> {
+            Animation anim= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+            binding.play.startAnimation(anim);
+            binding.play.setImageResource(R.drawable.scissors);
             String rockname = "rock";
             String papername = "paper";
             String scissorsname = "scissors";
@@ -61,26 +36,47 @@ public class MainActivity extends AppCompatActivity {
             Random random = new Random();
             index=random.nextInt(3);
             complay=game[index];
-            binding.texsresr.setText("me     " + yourplay + "     bot      "    + complay);
-
+            binding.texsresr.setText("you "+yourplay + " bot "    + complay);
             if (yourplay.equals(complay)) {
-                binding.natije.setText("tin");
+                binding.natije.setText("equal");binding.backgerand0.setImageResource(R.drawable.yeloo);
             } else if (yourplay.equals(rockname) && complay.equals(papername)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(rockname) && complay.equals(scissorsname)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
             } else if (yourplay.equals(papername) && complay.equals(rockname)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
             } else if (yourplay.equals(papername) && complay.equals(scissorsname)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(scissorsname) && complay.equals(rockname)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(scissorsname) && complay.equals(papername)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
+            }
+            if (complay.equals(papername)){
+                Animation anim33= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(anim33);
+                binding.rock.setImageResource(R.drawable.peper);
+            }else if (complay.equals(scissorsname)){
+                Animation anim44= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(anim44);
+                binding.rock.setImageResource(R.drawable.scissors);
+            }else if (complay.equals(rockname)){
+                Animation anim55= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(anim55);
+                binding.rock.setImageResource(R.drawable.rocks);
             }
 
         });
         binding.sang.setOnClickListener(view -> {
+            Animation anim= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+            binding.play.startAnimation(anim);
+            binding.play.setImageResource(R.drawable.rocks);
 
             String rockname = "rock";
             String papername = "paper";
@@ -93,26 +89,104 @@ public class MainActivity extends AppCompatActivity {
             Random random = new Random();
             index=random.nextInt(3);
             complay=game[index];
-            binding.texsresr.setText("me     " + yourplay + "     bot      "    + complay);
+            binding.texsresr.setText("you "+yourplay + " bot "    + complay);
 
             if (yourplay.equals(complay)) {
-                binding.natije.setText("tin");
+                binding.natije.setText("equal");
+                binding.backgerand0.setImageResource(R.drawable.yeloo);
             } else if (yourplay.equals(rockname) && complay.equals(papername)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(rockname) && complay.equals(scissorsname)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
             } else if (yourplay.equals(papername) && complay.equals(rockname)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
             } else if (yourplay.equals(papername) && complay.equals(scissorsname)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(scissorsname) && complay.equals(rockname)) {
-                binding.natije.setText("win bot");
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
             } else if (yourplay.equals(scissorsname) && complay.equals(papername)) {
-                binding.natije.setText("win");
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
+            }
+            if (complay.equals(papername)){
+                Animation ani6m= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(ani6m);
+                binding.rock.setImageResource(R.drawable.peper);
+            }else if (complay.equals(scissorsname)){
+                Animation ani7m= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(ani7m);
+                binding.rock.setImageResource(R.drawable.scissors);
+            }else if (complay.equals(rockname)){
+                Animation anima= AnimationUtils.loadAnimation(MainActivity.this,R.anim.fade_out);
+                binding.rock.startAnimation(anima);
+                binding.rock.setImageResource(R.drawable.rocks);
             }
 
         });
 
+        binding.kaghaz.setOnClickListener(view -> {
+            Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+            binding.play.startAnimation(anim);
+            binding.play.setImageResource(R.drawable.peper);
+            String rockname = "rock";
+            String papername = "paper";
+            String scissorsname = "scissors";
+            String[] game = {rockname, papername, scissorsname};
+            String complay;
+            String yourplay = ("paper");
+            int index;
+            binding.texsresr.setText(yourplay);
+            Random random = new Random();
+            index = random.nextInt(3);
+            complay = game[index];
+            binding.texsresr.setText("you " + yourplay + " bot " + complay);
+            if (yourplay.equals(complay)) {
+                binding.natije.setText("equal");
+                binding.backgerand0.setImageResource(R.drawable.yeloo);
+            } else if (yourplay.equals(rockname) && complay.equals(papername)) {
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
+            } else if (yourplay.equals(rockname) && complay.equals(scissorsname)) {
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
+            } else if (yourplay.equals(papername) && complay.equals(rockname)) {
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
+            } else if (yourplay.equals(papername) && complay.equals(scissorsname)) {
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
+            } else if (yourplay.equals(scissorsname) && complay.equals(rockname)) {
+                binding.natije.setText("bot win");
+                binding.backgerand0.setImageResource(R.drawable.red);
+            } else if (yourplay.equals(scissorsname) && complay.equals(papername)) {
+                binding.natije.setText("you win");
+                binding.backgerand0.setImageResource(R.drawable.grin);
+            }
+
+            if (complay.equals(papername)) {
+                Animation anim3 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+                binding.rock.startAnimation(anim3);
+                binding.rock.setImageResource(R.drawable.peper);
+            } else if (complay.equals(scissorsname)) {
+                Animation anim2 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+                binding.rock.startAnimation(anim2);
+                binding.rock.setImageResource(R.drawable.scissors);
+            } else if (complay.equals(rockname)) {
+                Animation anim1 = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_out);
+                binding.rock.startAnimation(anim1);
+                binding.rock.setImageResource(R.drawable.rocks);
+            }
+        });
+
+
+
 
         }
+
+
     }
